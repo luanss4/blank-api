d6df5ae48fb4ce67c0b1fa62d67f4e529eb7f054
C1293361 Luan Sousa Santana <c1293361@interno.bb.com.br>
2020-02-12 10:16:27 -0300

    first commit

---
+++ src/main/java/br/com/blankapi/BlankApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.blankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/blankapi/BlankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.blankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
